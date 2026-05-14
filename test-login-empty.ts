afc42e31d5924bbe6b98483d42f6d4d49d920fdc
v1shhh <ss1g3nji@gmail.com>
2026-05-14 22:13:27 +0400

    f

--- test-login-empty.ts
+++
@@ -1,5 +0,0 @@
-fetch("http://127.0.0.1:3000/api/admin/login", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({username: "", password: ""})
-}).then(res => res.json()).then(console.log).catch(console.error);
